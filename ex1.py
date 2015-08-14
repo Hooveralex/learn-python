75ea47154960f76c7d13e6a7d583fef0544d15e1
Alex Hoover <alex.hoover@kmstandards.com>
2015-08-13 21:53:11 -0400

    Finshed exercises 0 - 3

---
+++ ex1.py
@@ -0,0 +1,10 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "Another line."
+
+# 'print' prints out in the terminal. The hash mark comments out a line. So far, like Ruby
---
+++ ex3.py
@@ -0,0 +1,48 @@
+# + plus additions
+# - minus subtraction
+# / slash division
+# * asterisk multiplication
+# % percent remainder
+# < less-than
+# > greater-than
+# <= less-than-equal
+# >= greater-than-equal
+
+# Prints the string
+print "I will now count my chickens:"
+
+# Prints the string and then the result of the equation (30).
+# Note that the terminal does not print the coma. That is printed as a space.
+print "Hens", 25.0 + 30.0 / 6.0
+
+# Print string and number
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+# Print string
+print "Now I will count the eggs:"
+
+# Print number. Note that when you add the decimal after, the result changes. Without decimal, it is 7. With, 6.75.
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# Print just string. The comparison will be printed out as is bc it's in quotes
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Print true or false (false here) because it's a comparison
+print 3.0 + 2.0 < 5.0 - 7.0
+
+# Print string and number
+print "What is 3 + 2?", 3.0 + 2.0
+
+# print string and number
+print "What is 5 - 7?", 5.0 - 7.0
+
+# Print string
+print "Oh, that's why it's False."
+
+# Print string
+print "How about some more."
+
+# Print true or false
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
